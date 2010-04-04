22aae3062863583791458c5e3079443a55e33cdb
Patrick Hof <courts@offensivethinking.org>
2010-04-04 19:35:03 +0200

    Initial commit

---
+++ enc.rb
@@ -0,0 +1,224 @@
+#!/usr/bin/env ruby
+
+require 'cgi'
+require 'yaml'
+require 'digest/sha1'
+require 'digest/md5'
+
+# This module implements commonly used (string) encoders for various occasions.
+#
+# This software is licensed under the Creative
+# Commons CC0 1.0 Universal License.
+# To view a copy of this license, visit
+# http://creativecommons.org/publicdomain/zero/1.0/legalcode
+#
+# @author Patrick Hof
+module Enc
+
+  # Contains all encoders not fitting into a specific category.
+  module Std
+    # Returns a URL encoded string
+    #
+    # @param [String] data The data to URL encode
+    # @param [Boolean] all Whether to encode the whole string or only the
+    #   necessary parts (default is false)
+    # @return [String] the URL encoded string
+    def Std::url(data, all=false)
+      if all
+        data.split("").map do |x|
+          "%" + x.unpack('H*')[0]
+        end.join.upcase
+      else
+        CGI.escape(data)
+      end
+    end
+
+    # Returns the base64 representation of the given data
+    #
+    # @param [String] bin The data to encode in base64
+    # @return [String] Base64 encoded string of the given data
+    def Std::b64(bin)
+      [bin].pack("m").chomp
+    end
+
+    # XOR a string with another string
+    #
+    # Adapted from
+    # http://rubyforge.org/snippet/detail.php?type=snippet&id=108
+    #
+    # @param [String] data The original string
+    # @param [String] key The string to XOR with
+    # @return [String] the XOR'ed string
+    #
+    def Std::xor(data, key)
+      data.length.times {|x| data[x] = (data[x].ord ^ (key[x % key.length]).ord).chr }
+      return data
+    end
+
+    # Takes a string and returns the string with randomly uppercased characters
+    #
+    # @param [String] data The string to transform
+    # @return [String] The string with randomly uppercased characters
+    def Std::rand_upcase(data)
+      data.split("").map do |x|
+        if rand(2) == 0 then x.upcase else x end
+      end.join
+    end
+    # Returns the SHA1 encoded hex string of the given string
+    #
+    # @param [String] data The string to transform
+    # @return [String] SHA1 encoded string (hex)
+    def Std::sha1(data)
+      Digest::SHA1.hexdigest(data)
+    end
+
+    # Returns the MD5 encoded hex string of the given string
+    #
+    # @param [String] data The string to transform
+    # @return [String] MD5 encoded string (hex)
+    def Std::md5(data)
+      Digest::MD5.hexdigest(data)
+    end
+
+    # Returns the hex representation of data
+    #
+    # @param [String] data The data to transform
+    # @return [String] Hex representation of data
+    def Std::hex(data)
+      data.unpack('H*')[0].upcase
+    end
+  end
+
+  # Encoders converting strings to valid HTML.
+  module HTML
+    # HTML encodes a string
+    #
+    # @param [String] data The string to encode
+    # @return [String] The encoded string
+    def HTML::html(data)
+      CGI.escapeHTML(data)
+    end
+
+    # Decimal HTML encodes a string (e.g. "a" => &#97;
+    #
+    # @param [String] data The string to encode
+    # @return [String] The encoded string
+    def HTML::dec(data)
+      data.unpack("c*").map do |x|
+        "&##{x};"
+      end.join
+    end
+
+    # HTML hex encodes a string
+    #
+    # @param [String] data The string to encode
+    # @return [String] The encoded string
+    def HTML::hex(data)
+      data.split("").map do |x|
+        "&#x" + x.unpack('H*')[0].upcase + ";"
+      end.join
+    end
+  end
+
+  # UTF-8 encoders.
+  module UTF8
+    # UTF8 binary encodes a string
+    #
+    # @param [String] data The string to encode
+    # @return [String] The encoded string
+    def UTF8::bin(data)
+      data.split("").map do |x|
+        "\\x" + x.unpack('H*')[0].upcase
+      end.join
+    end
+
+    # UTF8 encodes a string
+    #
+    # @param [String] data The string to encode
+    # @return [String] The encoded string
+    def UTF8::utf8(data)
+      data.split("").map do |x|
+        val = x.unpack('H*')[0].upcase
+        if val.length == 2
+          "\\u00" + val
+        else
+          "\\u" + val
+        end
+      end.join
+    end
+  end
+
+  # Encoders returning valid MySQL strings.
+  module MySQL
+    # Encodes a string as MySQL CHAR, e.g. CHAR(98,99,100)
+    #
+    # @param [String] data The string to encode
+    # @return [String] The encoded string
+    def MySQL::char(data)
+      "CHAR(" + data.unpack("c*").join(",") + ")"
+    end
+
+    # Substitutes whitespace with MySQL comments .
+    #
+    # @param [String] data The statement to encode
+    # @return [String] the encoded statement
+    def MySQL::comment(data)
+      data.gsub!(/\s+/, "/**/")
+    end
+  end
+
+  # Encoders returning valid MSSQL strings.
+  module MSSQL
+    # Encodes a string as MSSQL CHARs, e.g. CHAR(98)+CHAR(99)+CHAR(100)
+    #
+    # @param [String] data The string to encode
+    # @return [String] The encoded string
+    def MSSQL::char(data)
+      data.unpack("c*").map do |x|
+        "CHAR(#{x})"
+      end.join("+")
+    end
+  end
+end
+
+
+##########################################################
+##########################################################
+if $0 == __FILE__
+  help_text = [
+    "",
+    "Available Encoders",
+    "------------------"
+  ]
+  Enc.constants.sort.each do |c|
+    Enc.const_get(c).singleton_methods.sort.each do |m|
+      help_text << "#{c}::#{m}"
+    end
+  end
+  help_text += [
+    "",
+    "Usage: #{__FILE__} <encoder> [params] <string from stdin>",
+    "",
+    "Examples:",
+    "#{__FILE__} Std::url <<< '<script>'",
+    "#{__FILE__} Std::url true <<< '<script>'",
+    "",
+    "Please see the YARD documentation for available parameters.",
+    "",
+    ""
+  ]
+  # Show help
+  if ARGV[0] == "-h"
+    puts help_text.join("\n")
+  # Run encoder 
+  else
+    begin
+      mod, m = ARGV[0].split("::")
+      params = ARGV[1] ? ARGV[1].split(",") : []
+      puts Enc.const_get(mod.to_sym).send(m.to_sym, $stdin.read(), *params)
+    rescue Exception => e
+      puts "ERROR: #{e}"
+      puts help_text.join("\n")
+    end
+  end
+end
---
+++ test/spec_enc.rb
@@ -0,0 +1,74 @@
+require 'bacon'
+require File.join(File.dirname(__FILE__), '..', 'enc')
+
+include Enc
+
+describe Enc do
+
+  before do
+    @url_text = "http://www.example.com?aa=bb&cc=dd"
+    @sql_text = "SELECT * FROM users WHERE id = '1' AND pass='password'"
+  end
+
+  it "should return a URL encoded string (either fully or only partially) when sent Std::url()" do
+    Std::url(@url_text).should.equal 'http%3A%2F%2Fwww.example.com%3Faa%3Dbb%26cc%3Ddd'
+    Std::url(@url_text, all=true).should.equal '%68%74%74%70%3A%2F%2F%77%77%77%2E%65%78%61%6D%70%6C%65%2E%63%6F%6D%3F%61%61%3D%62%62%26%63%63%3D%64%64'
+  end
+
+  it "should return a base64 representation of any binary data given to Std::b64()" do
+    Std::b64("test").should.equal "dGVzdA=="
+  end
+
+  it "should return a transformed string with randomly uppercased characters with Std::rand_upcase()" do
+    Std::rand_upcase(@url_text).downcase.should.equal @url_text
+  end
+
+  it "should return the SHA1 hex string when sent Std::sha1()" do
+    Std::sha1(@url_text).should.equal "ba53d1abf6b7c2fec072379fcd312920c02152c1"
+  end
+
+  it "should return the MD5 hex string when sent Std::md5()" do
+    Std::md5(@url_text).should.equal "052619cb4bae29861029b120f0b86d54"
+  end
+
+  it "should return the hex represenation of binary data if sent Std::hex()" do
+    Std::hex(@url_text).should.equal "687474703A2F2F7777772E6578616D706C652E636F6D3F61613D62622663633D6464"
+  end
+
+  it "should XOR two strings when sent Std::xor()" do
+    Std::xor(@url_text, "test").should.equal "\x1C\x11\a\x04NJ\\\x03\x03\x12]\x11\f\x04\x1E\x04\x18\x00]\x17\e\bL\x15\x15X\x11\x16R\x06\x10I\x10\x01"
+  end
+
+  it "should return an HTML escaped string when sent HTML::html()" do
+    HTML::html(@url_text).should.equal "http://www.example.com?aa=bb&amp;cc=dd"
+  end
+
+  it "should return the decimal html representation of a string when sent HTML::dec()" do
+    HTML::dec(@url_text).should.equal "&#104;&#116;&#116;&#112;&#58;&#47;&#47;&#119;&#119;&#119;&#46;&#101;&#120;&#97;&#109;&#112;&#108;&#101;&#46;&#99;&#111;&#109;&#63;&#97;&#97;&#61;&#98;&#98;&#38;&#99;&#99;&#61;&#100;&#100;"
+  end
+
+  it "should return the hexadecimal html representation of a string when sent HTML::hex()" do
+    HTML::hex(@url_text).should.equal '&#x68;&#x74;&#x74;&#x70;&#x3A;&#x2F;&#x2F;&#x77;&#x77;&#x77;&#x2E;&#x65;&#x78;&#x61;&#x6D;&#x70;&#x6C;&#x65;&#x2E;&#x63;&#x6F;&#x6D;&#x3F;&#x61;&#x61;&#x3D;&#x62;&#x62;&#x26;&#x63;&#x63;&#x3D;&#x64;&#x64;'
+  end
+
+  it "should return the UTF-8 binary representation of a string when sent UTF8::bin()" do
+    UTF8::bin(@url_text).should.equal '\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x65\x78\x61\x6D\x70\x6C\x65\x2E\x63\x6F\x6D\x3F\x61\x61\x3D\x62\x62\x26\x63\x63\x3D\x64\x64'
+  end
+
+  it "should return the UTF-8 representation of a string when sent UTF8::utf8()" do
+    UTF8::utf8(@url_text).should.equal '\u0068\u0074\u0074\u0070\u003A\u002F\u002F\u0077\u0077\u0077\u002E\u0065\u0078\u0061\u006D\u0070\u006C\u0065\u002E\u0063\u006F\u006D\u003F\u0061\u0061\u003D\u0062\u0062\u0026\u0063\u0063\u003D\u0064\u0064'
+  end
+  
+  it "should return a MySQL CHAR representation when sent MySQL::char()" do
+    MySQL::char(@sql_text).should.equal 'CHAR(83,69,76,69,67,84,32,42,32,70,82,79,77,32,117,115,101,114,115,32,87,72,69,82,69,32,105,100,32,61,32,39,49,39,32,65,78,68,32,112,97,115,115,61,39,112,97,115,115,119,111,114,100,39)'
+  end
+
+  it "should return a string with spaces swapped for comments when sent MySQL::comment()" do
+    MySQL::comment(@sql_text).should.equal "SELECT/**/*/**/FROM/**/users/**/WHERE/**/id/**/=/**/'1'/**/AND/**/pass='password'"
+  end
+
+  it "should return a MSSQL CHAR representation when sent MSSQL::char()" do
+    MSSQL::char(@sql_text).should.equal 'CHAR(83)+CHAR(69)+CHAR(76)+CHAR(69)+CHAR(67)+CHAR(84)+CHAR(32)+CHAR(42)+CHAR(32)+CHAR(70)+CHAR(82)+CHAR(79)+CHAR(77)+CHAR(32)+CHAR(117)+CHAR(115)+CHAR(101)+CHAR(114)+CHAR(115)+CHAR(32)+CHAR(87)+CHAR(72)+CHAR(69)+CHAR(82)+CHAR(69)+CHAR(32)+CHAR(105)+CHAR(100)+CHAR(32)+CHAR(61)+CHAR(32)+CHAR(39)+CHAR(49)+CHAR(39)+CHAR(32)+CHAR(65)+CHAR(78)+CHAR(68)+CHAR(32)+CHAR(112)+CHAR(97)+CHAR(115)+CHAR(115)+CHAR(61)+CHAR(39)+CHAR(112)+CHAR(97)+CHAR(115)+CHAR(115)+CHAR(119)+CHAR(111)+CHAR(114)+CHAR(100)+CHAR(39)'
+  end
+
+end
